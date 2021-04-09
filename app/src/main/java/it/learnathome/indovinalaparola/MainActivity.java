c1f78a57a4d9a8b16aa4c11480bdef13ee30a384
mrchip1975 <mrchip1975@gmail.com>
2021-04-09 11:30:50 +0200

    Progetto Android Indovina Parola Verifica Tentativo ed aggiornamento counter

--- app/src/main/java/it/learnathome/indovinalaparola/MainActivity.java
+++ app/src/main/java/it/learnathome/indovinalaparola/MainActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import it.learnathome.indovinalaparola.utils.GameMaster;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +19,18 @@ public class MainActivity extends AppCompatActivity {
     public void startGame(View v) {
         TextView shuffledTextLbl = findViewById(R.id.shuffledText);
         shuffledTextLbl.setText(GameMaster.startGame(MainActivity.this));
+        TextView yourProgressLbl = findViewById(R.id.yourProgressText);
+        yourProgressLbl.setText(GameMaster.buildCryptedWord());
+    }
+    public void checkAttempt(View v) {
+        counter++;
+        TextView attemptCounterLbl = findViewById(R.id.attemptCounter);
+        attemptCounterLbl.setText(String.valueOf(counter));
+        EditText myAttemptField = findViewById(R.id.myAttemptField);
+        String myAttemptFieldContent = myAttemptField.getText().toString();
+        String checkedResult = GameMaster.checkAttempt(myAttemptFieldContent);
+        TextView yourProgressLbl = findViewById(R.id.yourProgressText);
+        yourProgressLbl.setText(checkedResult);
+        myAttemptField.setText("");
     }
 }
--- app/src/main/java/it/learnathome/indovinalaparola/utils/GameMaster.java
+++ app/src/main/java/it/learnathome/indovinalaparola/utils/GameMaster.java
@@ -3,6 +3,7 @@ package it.learnathome.indovinalaparola.utils;
 import android.content.Context;
 
 import java.util.Random;
+import java.util.StringJoiner;
 
 public class GameMaster {
     private static String secretWord;
@@ -11,6 +12,13 @@ public class GameMaster {
     public static String startGame(Context ctx) {
         GameMaster.secretWord = WordSelector.getWord(ctx);
         return shuffleWord(GameMaster.secretWord);
+    }
+    public static String buildCryptedWord() {
+        String cryptedText="";
+        for (int i = 0; i < GameMaster.secretWord.length(); i++) {
+            cryptedText+="-";
+        }
+        return cryptedText;
     }
     private static String shuffleWord(String word) {
         char shuffledText[] = new char[word.length()*2];
@@ -29,5 +37,13 @@ public class GameMaster {
             position = selector.nextInt(vector.length);
         }
         return position;
+    }
+    public static String checkAttempt(String gamerAttempt) {
+        StringJoiner buffer = new StringJoiner("");
+        for (int i = 0; i < GameMaster.secretWord.length(); i++) {
+            buffer.add(i<gamerAttempt.length() && gamerAttempt.charAt(i)==GameMaster.secretWord.charAt(i)?
+               String.valueOf(GameMaster.secretWord.charAt(i)):"-");
+        }
+        return buffer.toString();
     }
 }
